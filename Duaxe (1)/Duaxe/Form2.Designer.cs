b0136fc94915318bc1a841ac6d788673947f3d5c
MinhNhut0810 <82552366+MinhNhut0810@users.noreply.github.com>
2021-05-13 14:08:40 +0700

    Add files via upload

---
+++ Duaxe (1)/Duaxe/Form2.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Duaxe
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Mycar = new System.Windows.Forms.PictureBox();
+            this.bien3 = new System.Windows.Forms.TextBox();
+            this.bien2 = new System.Windows.Forms.TextBox();
+            this.rock6 = new System.Windows.Forms.PictureBox();
+            this.rock5 = new System.Windows.Forms.PictureBox();
+            this.rock4 = new System.Windows.Forms.PictureBox();
+            this.lephai = new System.Windows.Forms.PictureBox();
+            this.lephai2 = new System.Windows.Forms.PictureBox();
+            this.letrai2 = new System.Windows.Forms.PictureBox();
+            this.over = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rock3 = new System.Windows.Forms.PictureBox();
+            this.rock2 = new System.Windows.Forms.PictureBox();
+            this.rock1 = new System.Windows.Forms.PictureBox();
+            this.letrai = new System.Windows.Forms.PictureBox();
+            this.bien1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Mycar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rock6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rock5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rock4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lephai)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lephai2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.letrai2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rock3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rock2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rock1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.letrai)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Mycar
+            // 
+            this.Mycar.Image = global::Duaxe.Properties.Resources.ảnh_xe_đua;
+            this.Mycar.Location = new System.Drawing.Point(167, 296);
+            this.Mycar.Name = "Mycar";
+            this.Mycar.Size = new System.Drawing.Size(34, 65);
+            this.Mycar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Mycar.TabIndex = 43;
+            this.Mycar.TabStop = false;
+            // 
+            // bien3
+            // 
+            this.bien3.Location = new System.Drawing.Point(177, -1);
+            this.bien3.Multiline = true;
+            this.bien3.Name = "bien3";
+            this.bien3.Size = new System.Drawing.Size(10, 84);
+            this.bien3.TabIndex = 42;
+            // 
+            // bien2
+            // 
+            this.bien2.Location = new System.Drawing.Point(177, 277);
+            this.bien2.Multiline = true;
+            this.bien2.Name = "bien2";
+            this.bien2.Size = new System.Drawing.Size(10, 84);
+            this.bien2.TabIndex = 41;
+            // 
+            // rock6
+            // 
+            this.rock6.Image = global::Duaxe.Properties.Resources.chướng_ngại_vật;
+            this.rock6.Location = new System.Drawing.Point(241, 12);
+            this.rock6.Name = "rock6";
+            this.rock6.Size = new System.Drawing.Size(41, 40);
+            this.rock6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.rock6.TabIndex = 40;
+            this.rock6.TabStop = false;
+            // 
+            // rock5
+            // 
+            this.rock5.Image = global::Duaxe.Properties.Resources.chướng_ngại_vật;
+            this.rock5.Location = new System.Drawing.Point(241, 130);
+            this.rock5.Name = "rock5";
+            this.rock5.Size = new System.Drawing.Size(41, 40);
+            this.rock5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.rock5.TabIndex = 39;
+            this.rock5.TabStop = false;
+            // 
+            // rock4
+            // 
+            this.rock4.Image = global::Duaxe.Properties.Resources.chướng_ngại_vật;
+            this.rock4.Location = new System.Drawing.Point(92, 277);
+            this.rock4.Name = "rock4";
+            this.rock4.Size = new System.Drawing.Size(41, 40);
+            this.rock4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.rock4.TabIndex = 38;
+            this.rock4.TabStop = false;
+            // 
+            // lephai
+            // 
+            this.lephai.Image = global::Duaxe.Properties.Resources.ảnh_lề_trái;
+            this.lephai.Location = new System.Drawing.Point(297, 154);
+            this.lephai.Name = "lephai";
+            this.lephai.Size = new System.Drawing.Size(55, 368);
+            this.lephai.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.lephai.TabIndex = 37;
+            this.lephai.TabStop = false;
+            // 
+            // lephai2
+            // 
+            this.lephai2.Image = global::Duaxe.Properties.Resources.ảnh_lề_trái;
+            this.lephai2.Location = new System.Drawing.Point(297, -207);
+            this.lephai2.Name = "lephai2";
+            this.lephai2.Size = new System.Drawing.Size(55, 368);
+            this.lephai2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.lephai2.TabIndex = 36;
+            this.lephai2.TabStop = false;
+            // 
+            // letrai2
+            // 
+            this.letrai2.Image = global::Duaxe.Properties.Resources.ảnh_lề_trái;
+            this.letrai2.Location = new System.Drawing.Point(-3, -222);
+            this.letrai2.Name = "letrai2";
+            this.letrai2.Size = new System.Drawing.Size(55, 368);
+            this.letrai2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.letrai2.TabIndex = 35;
+            this.letrai2.TabStop = false;
+            // 
+            // over
+            // 
+            this.over.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.over.BackColor = System.Drawing.Color.Yellow;
+            this.over.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.over.ForeColor = System.Drawing.Color.Red;
+            this.over.Location = new System.Drawing.Point(86, 93);
+            this.over.Name = "over";
+            this.over.Size = new System.Drawing.Size(182, 34);
+            this.over.TabIndex = 34;
+            this.over.Text = "GAME OVER";
+            this.over.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(68, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 16);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "Score: 0";
+            // 
+            // rock3
+            // 
+            this.rock3.Image = global::Duaxe.Properties.Resources.chướng_ngại_vật;
+            this.rock3.Location = new System.Drawing.Point(90, 71);
+            this.rock3.Name = "rock3";
+            this.rock3.Size = new System.Drawing.Size(38, 41);
+            this.rock3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.rock3.TabIndex = 32;
+            this.rock3.TabStop = false;
+            // 
+            // rock2
+            // 
+            this.rock2.Image = global::Duaxe.Properties.Resources.chướng_ngại_vật;
+            this.rock2.Location = new System.Drawing.Point(241, 220);
+            this.rock2.Name = "rock2";
+            this.rock2.Size = new System.Drawing.Size(41, 40);
+            this.rock2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.rock2.TabIndex = 31;
+            this.rock2.TabStop = false;
+            // 
+            // rock1
+            // 
+            this.rock1.Image = global::Duaxe.Properties.Resources.chướng_ngại_vật;
+            this.rock1.Location = new System.Drawing.Point(92, 173);
+            this.rock1.Name = "rock1";
+            this.rock1.Size = new System.Drawing.Size(40, 42);
+            this.rock1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.rock1.TabIndex = 30;
+            this.rock1.TabStop = false;
+            // 
+            // letrai
+            // 
+            this.letrai.Image = global::Duaxe.Properties.Resources.ảnh_lề_trái;
+            this.letrai.Location = new System.Drawing.Point(-3, 142);
+            this.letrai.Name = "letrai";
+            this.letrai.Size = new System.Drawing.Size(55, 368);
+            this.letrai.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.letrai.TabIndex = 29;
+            this.letrai.TabStop = false;
+            // 
+            // bien1
+            // 
+            this.bien1.Location = new System.Drawing.Point(177, 142);
+            this.bien1.Multiline = true;
+            this.bien1.Name = "bien1";
+            this.bien1.Size = new System.Drawing.Size(10, 84);
+            this.bien1.TabIndex = 28;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(346, 364);
+            this.Controls.Add(this.Mycar);
+            this.Controls.Add(this.bien3);
+            this.Controls.Add(this.bien2);
+            this.Controls.Add(this.rock6);
+            this.Controls.Add(this.rock5);
+            this.Controls.Add(this.rock4);
+            this.Controls.Add(this.lephai);
+            this.Controls.Add(this.lephai2);
+            this.Controls.Add(this.letrai2);
+            this.Controls.Add(this.over);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rock3);
+            this.Controls.Add(this.rock2);
+            this.Controls.Add(this.rock1);
+            this.Controls.Add(this.letrai);
+            this.Controls.Add(this.bien1);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form2_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Mycar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rock6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rock5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rock4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lephai)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lephai2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.letrai2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rock3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rock2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rock1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.letrai)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Mycar;
+        private System.Windows.Forms.TextBox bien3;
+        private System.Windows.Forms.TextBox bien2;
+        private System.Windows.Forms.PictureBox rock6;
+        private System.Windows.Forms.PictureBox rock5;
+        private System.Windows.Forms.PictureBox rock4;
+        private System.Windows.Forms.PictureBox lephai;
+        private System.Windows.Forms.PictureBox lephai2;
+        private System.Windows.Forms.PictureBox letrai2;
+        private System.Windows.Forms.Label over;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox rock3;
+        private System.Windows.Forms.PictureBox rock2;
+        private System.Windows.Forms.PictureBox rock1;
+        private System.Windows.Forms.PictureBox letrai;
+        private System.Windows.Forms.TextBox bien1;
+    }
+}
---
+++ Duaxe (1)/Duaxe/Form2.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Duaxe
+{
+    public partial class Form2 : Form
+    {
+        public int diemso;
+        public Form2()
+        {
+            InitializeComponent();
+            over.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (diemchoi < 200)
+            {
+                tocdo(5);
+                lose();
+                diemdatduoc();
+            }
+            else
+            {
+                if(diemchoi < 500)
+                {
+                    tocdo(8);
+                    lose();
+                    diemdatduoc();
+                }
+                else
+                {
+                    if (diemchoi < 1000)
+                    {
+                        tocdo(10);
+                        lose();
+                        diemdatduoc();
+                    }
+                }
+                
+            }
+            
+        }
+        Random newrock = new Random();
+        int diem1;
+        int diem2;
+        void tocdo(int speed)
+        {
+            if (lephai.Top > 300) lephai.Top = 0;
+            else lephai.Top += speed;
+            if (lephai2.Top > 300) lephai2.Top = 0;
+            else lephai2.Top += speed;
+            if (bien1.Top > 300) bien1.Top = 0;
+            else bien1.Top += speed;
+            if (bien2.Top > 300) bien2.Top = 0;
+            else bien2.Top += speed;
+            if (bien3.Top > 300) bien3.Top = 0;
+            else bien3.Top += speed;
+            if (letrai.Top > 300) letrai.Top = 0;
+            else letrai.Top += speed;
+            if (letrai2.Top > 300) letrai2.Top = 0;
+            else letrai2.Top += speed;
+            if (rock1.Top > 290)
+            {
+                diem1 = newrock.Next(60, 250);
+                Point tam = new Point(diem1, 0);
+                rock1.Location = tam;
+
+            }
+            else rock1.Top += speed;
+
+            if (rock2.Top > 290)
+            {
+                //diemchoi++;
+                diem2 = newrock.Next(60, 250);
+                while (Math.Abs(diem1 - diem2) <= 37) diem2 = newrock.Next(60, 250);
+                Point tam = new Point(diem2, 0);
+                rock2.Location = tam;
+
+            }
+            else rock2.Top += speed;
+
+            if (rock3.Top > 290)
+            {
+                //diemchoi++;
+                diem1 = newrock.Next(60, 250);
+                while (Math.Abs(diem1 - diem2) <= 37) diem1 = newrock.Next(60, 250);
+                Point tam = new Point(diem1, 0);
+                rock3.Location = tam;
+
+            }
+            else rock3.Top += speed;
+
+            if (rock4.Top > 290)
+            {
+                //diemchoi++;
+                diem2 = newrock.Next(60, 250);
+                while (Math.Abs(diem1 - diem2) <= 37) diem2 = newrock.Next(60, 250);
+                Point tam = new Point(diem2, 0);
+                rock4.Location = tam;
+
+            }
+            else rock4.Top += speed;
+
+            if (rock5.Top > 290)
+            {
+                //diemchoi++;
+                diem1 = newrock.Next(60, 250);
+                while (Math.Abs(diem1 - diem2) <= 37) diem1 = newrock.Next(60, 250);
+                Point tam = new Point(diem1, 0);
+                rock5.Location = tam;
+
+            }
+            else rock5.Top += speed;
+
+            if (rock6.Top > 290)
+            {
+                //diemchoi++;
+                diem2 = newrock.Next(60, 250);
+                while (Math.Abs(diem1 - diem2) <= 37) diem2 = newrock.Next(60, 250);
+                Point tam = new Point(diem2, 0);
+                rock6.Location = tam;
+
+            }
+            else rock6.Top += speed;
+            if (diemchoi == 200) speed++;
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        int speeds = 4;
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && Mycar.Left > 55) Mycar.Left -= 4;
+            if (e.KeyCode == Keys.Right && Mycar.Left < 255) Mycar.Left += 4;
+            if (e.KeyCode == Keys.Down && speeds > 4) speeds--;
+            if (e.KeyCode == Keys.Up && speeds < 50) speeds++;
+        }
+        int diemchoi = 0;
+        void diemdatduoc()
+        {
+            diemchoi++;
+            label1.Text = "Score:" + diemchoi.ToString() + " Km";
+        }
+        void lose()
+        {
+            if (Mycar.Bounds.IntersectsWith(rock1.Bounds))
+            {
+                timer1.Enabled = false;
+                over.Visible = true; // dieu kien de khi 2 pic cham nhau Pause- xuat hien Text Box
+                Form3 fr = new Form3(diemchoi.ToString());
+                this.Hide();
+                fr.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+
+                if (Mycar.Bounds.IntersectsWith(rock2.Bounds))
+                {
+                    timer1.Enabled = false;
+                    over.Visible = true;
+                    Form3 fr = new Form3(diemchoi.ToString());
+                    this.Hide();
+                    fr.ShowDialog();
+                    this.Show();
+                }
+                else
+                {
+                    if (Mycar.Bounds.IntersectsWith(rock3.Bounds))
+                    {
+                        timer1.Enabled = false;
+                        over.Visible = true;
+                        Form3 fr = new Form3(diemchoi.ToString());
+                        this.Hide();
+                        fr.ShowDialog();
+                        this.Show();
+                    }
+                    else
+                    {
+                        if (Mycar.Bounds.IntersectsWith(rock4.Bounds))
+                        {
+                            timer1.Enabled = false;
+                            over.Visible = true;
+                            Form3 fr = new Form3(diemchoi.ToString());
+                            this.Hide();
+                            fr.ShowDialog();
+                            this.Show();
+                        }
+                        else
+                        {
+                            if (Mycar.Bounds.IntersectsWith(rock5.Bounds))
+                            {
+                                timer1.Enabled = false;
+                                over.Visible = true;
+                                Form3 fr = new Form3(diemchoi.ToString());
+                                this.Hide();
+                                fr.ShowDialog();
+                                this.Show();
+                            }
+                            else
+                            {
+                                if (Mycar.Bounds.IntersectsWith(rock6.Bounds))
+                                {
+                                    timer1.Enabled = false;
+                                    over.Visible = true;
+                                    Form3 fr = new Form3(diemchoi.ToString());
+                                    this.Hide();
+                                    fr.ShowDialog();
+                                    this.Show();
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+
+        }
+        void exitgame()
+        {
+            if (MessageBox.Show("Do you exit?", "Notification", MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            exitgame();
+        }
+        private void newGameToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Form2 NewForm = new Form2();
+            NewForm.Show();
+            this.Dispose(false);
+        }
+    }
+}
